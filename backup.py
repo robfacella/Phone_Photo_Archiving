30a11e861eae584cb60962deaa7a0324ad9bdf26
BuildTools <unconfigured@null.spigotmc.org>
2024-04-13 19:06:55 -0400

    Download a File from List in Phone to Location on PC

--- backup.py
+++ backup.py
@@ -1,6 +1,8 @@
 import ftplib
 from ftplib import FTP
 
+import os
+
 # Going to be local to YOU
 phoneIP='192.168.1.152'
 # Probably Arbitrary
@@ -8,7 +10,10 @@ port=7871
 # Default for FileManager+
 username='pc'
 # Set to Random, should read in as argument in future
-passwort = '974316'
+passwort = '398163'
+
+# Where on local File System you want to Backup the Phone to
+backupLocation="/media/rob/38B62D40724FA264/phone/PIXEL"
 
 # Create an FTP object
 ftp = FTP()
@@ -20,6 +25,9 @@ ftp.connect(phoneIP, port)
 # Give Login Credentials
 ftp.login (user=username, passwd=passwort)
 # acct='', timeout=None, source_address=None, *, encoding='utf-8' )
+
+# Max Debug Mode
+#ftp.set_debuglevel(2)
 
 #ftp.getwelcome()
 # There is None for Filemanager Plus
@@ -42,6 +50,7 @@ def vCD(dir):
 def getFileList():
 	filenames = []
 	ftp.retrlines('NLST', filenames.append)
+	print ( "Items in List " + str(len(filenames)) )
 	return filenames
 verboseList()
 
@@ -54,5 +63,21 @@ vCD("DCIM/Camera")
 
 filenames=getFileList()
 
-for files in filenames:
-	print ( files )
+#for files in filenames:
+#	print ( files )
+print ( "now Just print files 0 and 1 ")
+print (filenames[0])
+print (filenames[1])
+
+print ( "Local dir: " + os.getcwd() )
+#os.getcwd()
+os.chdir(backupLocation)
+print ( "Local dir: " + os.getcwd() )
+
+# Single File Backup
+# Write file in binary mode
+with open(filenames[0], "wb") as file:
+    # Command for Downloading the file "RETR filename"
+    ftp.retrbinary(f"RETR {filenames[0]}", file.write)
+# Does not preserve Modified Timestamp
+# Also seems to Write EVERY time
