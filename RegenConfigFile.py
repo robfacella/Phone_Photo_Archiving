d9d84687c444feb165c6a0dc68a749487e6edb26
robfacella <rob.facella@gmail.com>
2024-12-01 14:42:35 -0500

    Replace Template

---
+++ RegenConfigFile.py
@@ -0,0 +1,3 @@
+#!/bin/python3
+with open("Config.txt", "w") as f:
+	f.writelines(["LocalDir=/media/rob/38B62D40724FA264/phone/PIXEL\n", "RemoteDir=device/DCIM/Camera\n", "ipAddr=192.168.1.153\n", "port=7871\n", "username=SAMPLE\n", "password=DANGER\n"])
