57501d06ddd52cf6c466d07aa01561350710ce42
robfacella <rob.facella@gmail.com>
2024-12-15 21:05:48 -0500

    Quit on Not Root

--- WhoAreYou.py
+++ WhoAreYou.py
@@ -1,13 +1,17 @@
 from subprocess import PIPE, run
+import sys
 
-command = ['whoami']
-result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+def QuitIfNotRoot() :
+	command = ['whoami']
+	result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+	#print (result.returncode, result.stdout, result.stderr)
+	iam = result.stdout
+	iam = iam.strip()
+	if iam != 'root' :
+		print (f"Hello, {iam}! You are not root")
+		sys.exit("Not Root, exiting...")
+	if iam == 'root' :
+		print (f"Welcome home, {iam},")
 
-#print (result.returncode, result.stdout, result.stderr)
-iam = result.stdout
-iam = iam.strip()
-
-if iam != 'root' :
-	print (f"Hello, {iam}! You are not root")
-if iam == 'root' :
-	print (f"Welcome home, {iam},")
+QuitIfNotRoot()
+print ("Did not Quit")
