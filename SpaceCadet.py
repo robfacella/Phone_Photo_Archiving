7443fcddef6c6c93c87f5f069da9e29f9fddee21
robfacella <rob.facella@gmail.com>
2025-01-26 21:30:44 -0500

    Doesn't change time despite being offline...

--- SpaceCadet.py
+++ SpaceCadet.py
@@ -10,3 +10,14 @@ def RunSP(Command):
                 result = subprocess.run(Command, capture_output=True, text=True)
         print (result.stdout)
         return ( result )
+def TimeDilation(Command):
+        CommandWords = Command.split(" ")
+        newWords = [CommandWords[0], CommandWords[1], f"\"{CommandWords[2]} {CommandWords[3]}\""]
+        CommandWords = newWords
+        print (f"{CommandWords}")
+        if len(CommandWords) > 1:
+                result = subprocess.run(CommandWords, capture_output=True, text=True)
+        else:
+                result = subprocess.run(Command, capture_output=True, text=True)
+        print (result.stdout)
+        return ( result )
--- TimeTwister.py
+++ TimeTwister.py
@@ -13,14 +13,34 @@ def QuitIfNotRoot() :
 		sys.exit("Not Root, exiting...")
 	if iam == 'root' :
 		print (f"Welcome home, {iam},")
+'''
 def TimeStatus01() :
 	command = ['timedatectl status']
 	result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 	print (result.returncode, result.stdout, result.stderr)
 	StatusCaptain = result.stdout
 	print (f"{StatusCaptain}")
+'''
 def TimeStatus():
 	SpaceCadet.RunSP("timedatectl status")
+def DesyncTimeline():
+	SpaceCadet.RunSP("systemctl stop systemd-timesyncd.service")
+	SpaceCadet.RunSP("timedatectl set-ntp 0")
+def ResyncTimeline():
+	SpaceCadet.RunSP("systemctl start systemd-timesyncd.service")
+	SpaceCadet.RunSP("timedatectl set-ntp 1")
+def TimeWarp(DateTime):
+	SpaceCadet.TimeDilation(f"timedatectl set-time {DateTime}")
+
 QuitIfNotRoot()
 print ("Did not Quit")
+TimeStatus()
+DesyncTimeline()
+TimeStatus()
+
+TimeWarp("2001-09-11 07:59:00")
+
+TimeStatus()
+print("Success!")
+ResyncTimeline()
 TimeStatus()
